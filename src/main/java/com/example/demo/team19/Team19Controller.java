de02371456ee9984d99e546caa899f00fe74b820
ko_matsumoto <ko_matsumoto@bold.ne.jp>
2026-05-20 10:15:02 +0900

    作成 サブコントローラー

--- src/main/java/com/example/demo/team19/Team19Controller.java
+++ src/main/java/com/example/demo/team19/Team19Controller.java
@@ -1,8 +1,10 @@
 package com.example.demo.team19;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class Team19Controller {
 
 	@GetMapping("/team19")
@@ -11,8 +13,13 @@ public class Team19Controller {
 	}
 	
 	@PostMapping("/team19")
-	public String send() {
+	public String send1() {
 		return "Team19Main";
 	}
+	@PostMapping("/team19")
+	public String send2() {
+		return "Team19Sub";
+	}
+	
 }
 	
