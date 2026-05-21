64c5ca2d318b290954d7e5fe5a7007eb5de63dfe
ono <ka_ono@bold.ne.jp>
2026-05-21 10:05:24 +0900

    作成 全画面遷移ボタン コントローラーへのコメント文

--- src/main/java/com/example/demo/team19/Team19Controller.java
+++ src/main/java/com/example/demo/team19/Team19Controller.java
@@ -6,40 +6,71 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Team19Controller {
-
+	//最初にアクセスした時
 	@GetMapping("/team19")
 	public String  index() {
-		return "team19/Team19Top";
+		return "team19/Team19Home";
 	}
 	
+	//ようこそボタン
 	@PostMapping("/team19_1")
 	public String send1() {
-		return "team19/Team19Main";
+		return "team19/Team19Mood";
 	}
 	
+	//登録ボタン
 	@PostMapping(value="/team19_1",params="register")
 	public String send4() {
 		return "team19/Team19Register";
 	}
+	
+	//気分選択
 	@PostMapping("/team19_2")
 	public String send2() {
-		return "team19/Team19Sub";
-	}
-	@PostMapping(value="/team19_2", params="back")
-	public String sendback2() {
-		return "team19/Team19Top";
+		return "team19/Team19Janru";
 	}
 	
+	//気分選択画面からホームへ戻るボタン
+	@PostMapping(value="/team19_2", params="back")
+	public String sendback2() {
+		return "team19/Team19Home";
+	}
+	
+	//ジャンル選択
 	@PostMapping("/team19_3")
 	public String send3() {
 		return "team19/Team19Result";
 	}
 	
+	//ジャンル選択画面から気分画面へ戻るボタン
 	@PostMapping(value="/team19_3", params="back")
 	public String sendback3() {
-		return "team19/Team19Main";
+		return "team19/Team19Mood";
 	}
-
+	
+	//曲追加ボタン
+	@PostMapping(value="/team19_4", params="add")
+	public String add() {
+		return "team19/Team19Register";
+	}
+	
+	//曲登録確定ボタン
+	@PostMapping(value="/team19_4", params="register")
+	public String send5() {
+		return "team19/Team19RegisterResult";
+	}
+	
+	//登録画面からホーム画面へ戻るボタン
+	@PostMapping(value="/team19_4", params="back")
+	public String sendback5() {
+		return "team19/Team19Home";
+	}
+	
+	//登録結果画面から登録画面へ戻るボタン
+	@PostMapping(value="/team19_5", params="back")
+	public String sendback6() {
+		return "team19/Team19Register";
+	}
 	
 }
 	
