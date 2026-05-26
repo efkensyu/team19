cbf2456c97eae535f2f8781aba0735cefb79f59c
ko_matsumoto <ko_matsumoto@bold.ne.jp>
2026-05-26 10:55:19 +0900

    分割　Controller.java

--- src/main/java/com/example/demo/team19/Team19Controller.java
+++ src/main/java/com/example/demo/team19/Team19Controller.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 
 import com.example.demo.team19.team19Comment.Team19Comment;
 import com.example.demo.team19.team19Comment.Team19CommentService;
@@ -163,51 +162,5 @@ public class Team19Controller {
 			return "team19/Team19Register";
 		}
 		
-	//曲追加ボタン
-	@PostMapping(value="/team19_4", params="add")
-	public String add(@ModelAttribute @Validated Team19RegisterForm team19RegisterForm,BindingResult result,Model model,SessionStatus sessionStatus) {
-		if(result.hasErrors()) {
-			//model.addAttribute("team19RegisterForm",new Team19RegisterForm());
-			return "team19/Team19Register";
-		}
-		registerlist.add(team19RegisterForm);
-		model.addAttribute("team19RegisterForm",new Team19RegisterForm());
-		model.addAttribute("result",registerlist);
-		return "team19/Team19Register";
-	}
-	
-	//曲登録画面の「削除」ボタン
-	@PostMapping(value="/team19_4", params="clear")
-	public String clear(@ModelAttribute Team19RegisterForm team19RegisterForm,Model model,SessionStatus sessionStatus) {
-		model.addAttribute("team19RegisterForm",new Team19RegisterForm());
-		registerlist.clear();
-		return "team19/Team19Register";
-	}
-	
-	//曲登録確定ボタン
-	@PostMapping(value="/team19_4", params="register")
-	public String send5(@ModelAttribute Team19RegisterForm team19RegisterForm,BindingResult result, Model model,SessionStatus sessionStatus) {
-		if(registerlist == null || registerlist.isEmpty()) {
-			result.reject("emptyList", "登録データがありません");
-			return "team19/Team19Register";
-		}
-		//テーブル追加処理
-		for(Team19RegisterForm d : registerlist) {
-			musicService.insertMusic(d.getMusicNm(), d.getArtist(), d.getJanru(), d.getUrl(), d.getMood());
-		}
-		//セッションのリフレッシュ
-		sessionStatus.setComplete();
-		model.addAttribute("result", registerlist);
-		return "team19/Team19RegisterResult";
-	}
-	
-	
-	//曲登録画面からホーム画面へ戻るボタン
-	@PostMapping(value="/team19_4", params="back")
-	public String sendback5() {
-		registerlist.clear();
-		return "team19/Team19Home";
-	}
 }
-
 	
